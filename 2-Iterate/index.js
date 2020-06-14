6577b0acdba6442b44580efd8c5f6d808d19b9ce
DoLim <limdongyoung0@gmail.com>
2020-06-14 11:12:18 +0000

    study: 20200614 iterable / iterator protocol

--- 2-Iterate/index.js
+++ 2-Iterate/index.js
@@ -27,3 +27,37 @@ for (const a of str) {
 /**
  * Array를 통해 알아보기
  */
+
+log("Arr -----------------");
+const arr = [1, 2, 3];
+log(arr[Symbol.iterator]);
+for (const a of arr) log(a);
+
+/**
+ * Set을 통해 알아보기
+ */
+
+log("Set -----------------");
+const set = new Set([1, 2, 3]);
+for (const a of set) log(a);
+
+/**
+ * Map을 통해 알아보기
+ */
+
+log("Map -----------------");
+const map = new Map([
+  ["a", 1],
+  ["b", 2],
+  ["c", 3],
+]);
+for (const a of map.keys()) log(a);
+for (const a of map.values()) log(a);
+for (const a of map.entries()) log(a);
+
+/**
+ * ## 이터러블 / 이터레이터
+ * - 이터러블 : 이터레이터를 리턴하는 [Symbol.iterator]() 를 가진 값. 이터러블[Symbol.iterator]()를 실행하면 이터레이터가 리턴된다는 뜻
+ * - 이터레이터 : { value, done } 객체를 리턴하는 next() 를 가진 값. 이터레이터.next()를 호출하면 { value, done }가 리턴된다는 뜻
+ * - 이터러블 / 이터레이터 프로토콜 : 이터러블을 for ... of, 전개 연산자(...) 등과 함께 동작하도록 하는 프로토콜(규약)
+ */
