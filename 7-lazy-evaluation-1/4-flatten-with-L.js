ef12b4862d17b8dcf7bad6f06c3ed4bcf3830707
DoLim <limdongyoung0@gmail.com>
2020-08-05 04:58:08 +0000

    study: 20200805 studied deepFlat

--- 7-lazy-evaluation-1/4-flatten-with-L.js
+++ 7-lazy-evaluation-1/4-flatten-with-L.js
@@ -41,3 +41,12 @@ log(take(6, L.flatten([[1, 2], 3, 4, [5, 6], [7, 8, 9]])));
 
 const flatten = pipe(L.flatten, takeAll);
 log(flatten([[1, 2], 3, 4, [5, 6], [7, 8, 9]]));
+
+L.deepFlat = function* f(iter) {
+  for (const a of iter) {
+    if (isIterable(a)) yield* f(a);
+    else yield a;
+  }
+};
+
+log([...L.deepFlat([1, [2, [3, 4], [[5]]]])]);
